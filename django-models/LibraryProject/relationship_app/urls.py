a031fc143795318d8978a3dd7dd23a562dad7197
Peter Ayad <eng.peter2013@gmail.com>
2025-07-17 12:33:19 -0700

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import list_books
+from .views import book_list
 from .views import LibraryDetailView
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # Function-based view for listing all books
-    path('books/', list_books, name='list_books'),
+    path('books/', book_list, name='list_books'),
     
     # Class-based view for displaying library details
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
@@ -14,6 +14,12 @@ urlpatterns = [
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register.as_view(), name='register'),
+
+    # Role-based URLs
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.views.generic.detail import DetailView, ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Book
 from .models import Library
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -41,4 +42,26 @@ class LoginView(LoginView):
 def LogoutView(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+
+def check_role(user, role):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == role
+
+def role_required(role):
+    return user_passes_test(lambda u: check_role(u, role))
+
+@login_required
+@role_required('Admin')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@role_required('Librarian')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@role_required('Member')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
