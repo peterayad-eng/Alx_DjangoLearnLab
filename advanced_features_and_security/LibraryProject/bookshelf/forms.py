d980c5929faa1d5fc86f10c02ff94c46e12d87ee
Peter Ayad <eng.peter2013@gmail.com>
2025-07-25 04:07:03 -0700

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -14,3 +14,15 @@ class BookForm(forms.ModelForm):
             'publication_year': forms.NumberInput(attrs={'min': 0, 'max': 9999}),
         }
 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+        labels = {
+            'title': 'Book Title',
+            'author': 'Author Name',
+            'publication_year': 'Year of Publication'
+        }
+        widgets = {
+            'publication_year': forms.NumberInput(attrs={'min': 0, 'max': 9999}),
+        }
