a0bea0be247b5a1abae68ed2f6067334a034dac0
Peter Ayad <eng.peter2013@gmail.com>
2025-08-21 07:00:42 -0700

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 
+# feed/
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
